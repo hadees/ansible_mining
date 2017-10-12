98aef81e54eb0d792d312851fee1ebf63b4eaadb
KemoNine <mcrosson_github@kemonine.info>
2017-10-12 13:56:34 -0400

    Build out setup.sh to look for env vars for auto-starting mining after Ansible runs (useful for cloud and auto-deploys)

--- setup.sh
+++ setup.sh
@@ -11,4 +11,27 @@ sudo chmod 777 /ansible_mining
 sudo ansible-playbook -i /ansible_mining/inventory/localhost.yml /ansible_mining/playbook_mining.yml
 
 # Run any mining that didn't auto-start via Ansible
-sudo systemctl start sc-marlin
+if [[ -z "${SC_MARLIN}" ]]; then
+    sudo systemctl start sc-marlin
+fi
+if [[ -z "${LBRY_CCMINER_ALT}" ]]; then
+    sudo systemctl start lbry-ccminer-alt
+fi
+if [[ -z "${XMR_XDN_CCMINER_CRYPTONIGHT}" ]]; then
+    sudo systemctl start xmr-xdn-ccminer-cryptonight
+fi
+if [[ -z "${SC_CCMINER}" ]]; then
+    sudo systemctl start sc-ccminer
+fi
+if [[ -z "${ETH_SIA_CLAYMORE}" ]]; then
+    sudo systemctl start eth-sia-claymore
+fi
+if [[ -z "${MUSIC_SC_CLAYMORE}" ]]; then
+    sudo systemctl start music-sc-claymore
+fi
+if [[ -z "${ZEC_EBWF}" ]]; then
+    sudo systemctl start zec-ebwf
+fi
+if [[ -z "${BCN_XMR_MINERGATE}" ]]; then
+    sudo systemctl start bcn-xmr-minergate
+fi
