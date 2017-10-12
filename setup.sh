b84ef7f75bc9ba6e9e58429687bcc7254e697814
KemoNine <mcrosson_github@kemonine.info>
2017-10-12 15:32:04 -0400

    Fixup conditionals in setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+env # Handy for debugging cloud deployments
+
 # Add ansible and install
 sudo add-apt-repository --yes ppa:ansible/ansible
 sudo apt-get --assume-yes update
@@ -11,27 +13,27 @@ sudo chmod 777 /ansible_mining
 sudo ansible-playbook -i /ansible_mining/inventory/localhost.yml /ansible_mining/playbook_mining.yml
 
 # Run any mining that didn't auto-start via Ansible
-if [[ -z "${SC_MARLIN}" ]]; then
+if [ "${SC_MARLIN}" = true ]; then
     sudo systemctl start sc-marlin
 fi
-if [[ -z "${LBRY_CCMINER_ALT}" ]]; then
+if [ "${LBRY_CCMINER_ALT}"  = true ]; then
     sudo systemctl start lbry-ccminer-alt
 fi
-if [[ -z "${XMR_XDN_CCMINER_CRYPTONIGHT}" ]]; then
+if [ "${XMR_XDN_CCMINER_CRYPTONIGHT}" = true ]; then
     sudo systemctl start xmr-xdn-ccminer-cryptonight
 fi
-if [[ -z "${SC_CCMINER}" ]]; then
+if [ "${SC_CCMINER}" = true ]; then
     sudo systemctl start sc-ccminer
 fi
-if [[ -z "${ETH_SIA_CLAYMORE}" ]]; then
+if [ "${ETH_SIA_CLAYMORE}" = true ]; then
     sudo systemctl start eth-sia-claymore
 fi
-if [[ -z "${MUSIC_SC_CLAYMORE}" ]]; then
+if [ "${MUSIC_SC_CLAYMORE}" = true ]; then
     sudo systemctl start music-sc-claymore
 fi
-if [[ -z "${ZEC_EBWF}" ]]; then
+if [ "${ZEC_EBWF}" = true ]; then
     sudo systemctl start zec-ebwf
 fi
-if [[ -z "${BCN_XMR_MINERGATE}" ]]; then
+if [ "${BCN_XMR_MINERGATE}" = true ]; then
     sudo systemctl start bcn-xmr-minergate
 fi
